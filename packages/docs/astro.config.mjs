fde3f7bf934e955eabdf621fab8ef5833d8f0b2b
rxliuli <rxliuli@gmail.com>
2026-01-29 17:22:03 +0800

    docs: update deploy config

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -40,4 +42,6 @@ export default defineConfig({
       ],
     }),
   ],
+
+  adapter: cloudflare(),
 })
